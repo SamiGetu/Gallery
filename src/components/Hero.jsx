1ca7f9adfbfcc07465330d10199569b1da9abc26
Samuel Getachew <falconroman36@gmail.com>
2024-02-04 15:04:06 +0300

    commited

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/prop-types */
+import PropTypes from "prop-types";
 import Socials from "./socials";
 
 const Hero = ({ style, Hero_H1, Hero_layout, Hero_P, Button }) => {
@@ -7,13 +7,21 @@ const Hero = ({ style, Hero_H1, Hero_layout, Hero_P, Button }) => {
       <Socials />
       <div className={style}>
         <div className={Hero_layout}>
-          <h1 className="text-7xl  font-bold">{Hero_H1}</h1>
+          <h1 className="text-7xl font-bold">{Hero_H1}</h1>
           <p className="text-2xl pb-10">{Hero_P}</p>
           {Button}
         </div>
       </div>
     </>
   );
+};
+
+Hero.propTypes = {
+  style: PropTypes.string.isRequired,
+  Hero_H1: PropTypes.string.isRequired,
+  Hero_layout: PropTypes.string.isRequired,
+  Hero_P: PropTypes.string.isRequired,
+  Button: PropTypes.element.isRequired,
 };
 
 export default Hero;
