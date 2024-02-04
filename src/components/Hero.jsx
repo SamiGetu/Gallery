dab610aa9a00213f1d61f21d1f06cb5f09a50eb9
Samuel Getachew <falconroman36@gmail.com>
2024-02-04 15:07:37 +0300

    commited

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,8 @@
 import PropTypes from "prop-types";
-import Socials from "./socials";
 
 const Hero = ({ style, Hero_H1, Hero_layout, Hero_P, Button }) => {
   return (
     <>
-      <Socials />
       <div className={style}>
         <div className={Hero_layout}>
           <h1 className="text-7xl font-bold">{Hero_H1}</h1>
