1afb6eb230ec706f50c54222285b9a131ade08ae
Samuel Getachew <falconroman36@gmail.com>
2024-02-04 14:54:03 +0300

    commited

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,19 @@
 /* eslint-disable react/prop-types */
 import Socials from "./socials";
 
-export default function Hero(props) {
+const Hero = ({ style, Hero_H1, Hero_layout, Hero_P, Button }) => {
   return (
     <>
       <Socials />
-      <div className={props.style}>
-        <div className={props.Hero_layout}>
-          <h1 className="text-7xl  font-bold">{props.Hero_H1}</h1>
-          <p className="text-2xl pb-10">{props.Hero_P}</p>
-          {props.Button}
+      <div className={style}>
+        <div className={Hero_layout}>
+          <h1 className="text-7xl  font-bold">{Hero_H1}</h1>
+          <p className="text-2xl pb-10">{Hero_P}</p>
+          {Button}
         </div>
       </div>
     </>
   );
-}
+};
+
+export default Hero;
